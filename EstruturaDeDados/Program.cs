cafb41ba9ceba02d3449aa9f6ff549c88fbf9514
ELOÁ SILVA <francodosreissilvae@gmail.com>
2024-08-21 15:22:38 -0300

    FINALIZADO O EXEMPLO DE DICIONARIO DE DADOS

--- EstruturaDeDados/Program.cs
+++ EstruturaDeDados/Program.cs
@@ -13,10 +13,7 @@
 
         /*IMPRIMIR OS ITENS DA LISTA*/
         Console.WriteLine("Minha Lista de Frutas:");
-        foreach (string fruta in frutas) ;
-        {
-            Console.WriteLine(fruta);
-        }
+        frutas.ForEach(Console.WriteLine);
         /*IMPRIMIR ELEMENTO NO INDICE ESPECIFICA*/
         Console.WriteLine("Fruta na indice 2:" + frutas[2]);
 
@@ -27,27 +24,43 @@
         Console.WriteLine(); //PULA LINHA EM BRANCO 
 
         Console.WriteLine("Minha Lista de frutas:");
-        foreach (string fruta in frutas)
-        {
-            Console.WriteLine(fruta);
-        }
+        frutas.ForEach(Console.WriteLine);
+
         /*ALTERNAR UM ELEMENTO NO ÍNDICE ESPECIFICO*/
         frutas[4] = "Pêra";
         /*IMPRIMINDO A LISTA NOVAMENTE */
         Console.WriteLine();
         Console.WriteLine("Minha lista de fruta:");
-        foreach (string fruta in frutas) ;
-        {
-            Console.WriteLine(fruta);
-        }
+        frutas.ForEach(Console.WriteLine);
+
         /*REMOVER ELEMENTOS DA LISTA NO INDICE ESPECÍFICO*/
         frutas.Remove("Morango");
         /*IMPRIMINDO A LISTA NOVAMENTE */
         Console.WriteLine();
         Console.WriteLine("Minha lista de fruta:");
         frutas.ForEach(Console.WriteLine);
-   
+
         /* APAGAR TODOS OS ELEMENTOS DA LISTA */
         frutas.Clear();
+
+        //TRABALHANDO COM DICIONARIO (DICIONARY)
+        /* CRIANDO UM DICIONARIO DE DADOS */
+        Dictionary<int, string> carros = new Dictionary<int, string>();
+        /* ADICIONAR DADOS E UM DICIONARIO*/
+        carros.Add(5, "Corsa");
+        carros.Add(10, "fusca");
+        carros.Add(2, "Ford ka");
+
+        Console.WriteLine();//PULA LINHA EM BRANCO
+        Console.WriteLine("===============================)"; 
+        Console.WriteLine();//PULA LINHA EM BRANCO
+
+        Console.WriteLine("Meu Dicionario de Carros:");
+        /*IMPRIMIR UM DICIONARIO  DE DADOS */
+        foreach (var carro in carros)
+        {
+            Console.WriteLine($"{carro.Key} - {carro.Value}");
+        }
+
     }
 }
